8d0726a7ab93205cc0600e20f448d4a372328573
vsuraj25 <55409076+vsuraj25@users.noreply.github.com>
2022-12-15 10:59:17 +0530

    Form Response and Api Testing Complete

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -75,6 +75,14 @@ def api_response(dict_request):
             response = {'response': response}
             return response
 
-    except Exception as e:
+    except NotInRange as e:
         response = {"the_expected_range": get_schema(), 'response': str(e)}
+        return response
+
+    except NotInCols as e:
+        response = {"the_expected_cols": get_schema().keys(), 'response': str(e)}
+        return response
+
+    except Exception as e:
+        response = {'response': str(e)}
         return response
